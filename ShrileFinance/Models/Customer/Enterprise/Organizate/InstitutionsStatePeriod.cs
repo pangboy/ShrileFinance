a0e9fee890ca1ed346e035a7e753ce29caa2b52e
邹强鲁 <Thinkxuhao@outlook.com>
2016-10-24 20:19:32 +0800

    文档2.2 机构信息与家族相关页面

--- ShrileFinance/Models/Customer/Enterprise/Organizate/InstitutionsStatePeriod.cs
+++ ShrileFinance/Models/Customer/Enterprise/Organizate/InstitutionsStatePeriod.cs
@@ -20,7 +20,7 @@ namespace Models.Customer.Enterprise.Organizate
         /// <summary>
         /// 信息记录类型
         /// </summary>
-        [Display(Name = "信息记录类型"), StringLength(1), Required, AN(ErrorMessage = "信息记录类型类型错误")]
+        [Display(Name = "信息记录类型"), StringLength(1), Required, AN(ErrorMessage = "信息记录类型 类型错误")]
         public string InformationCategories
         {
             get
@@ -32,31 +32,31 @@ namespace Models.Customer.Enterprise.Organizate
         /// <summary>
         /// 基本户状态
         /// </summary>
-        [Display(Name = "基本户状态"), StringLength(1), AN(ErrorMessage = "基本户状态类型错误")]
+        [Display(Name = "基本户状态"), StringLength(1), AN(ErrorMessage = "基本户状态 类型错误")]
         public string BasicAccountState { get; set; }
 
         /// <summary>
         /// 企业规模
         /// </summary>
-        [Display(Name = "企业规模"), StringLength(1), AN(ErrorMessage = "企业规模类型错误")]
+        [Display(Name = "企业规模"), StringLength(1), AN(ErrorMessage = "企业规模 类型错误")]
         public string EnterpriseScale { get; set; }
 
         /// <summary>
         /// 机构状态
         /// </summary>
-        [Display(Name = "机构状态"), StringLength(1), AN(ErrorMessage = "机构状态类型错误")]
+        [Display(Name = "机构状态"), StringLength(1), AN(ErrorMessage = "机构状态 类型错误")]
         public string InstitutionsState { get; set; }
 
         /// <summary>
         /// 信息更新日期
         /// </summary>
-        [Display(Name = "信息更新日期"), StringLength(8), Required, N(ErrorMessage = "信息更新日期类型错误")]
+        [Display(Name = "信息更新日期"), StringLength(8), Required, N(ErrorMessage = "信息更新日期 类型错误")]
         public string InformationUpdateDate { get; set; }
 
         /// <summary>
         /// 预留字段
         /// </summary>
-        [Display(Name = "预留字段"), StringLength(40), ANC(ErrorMessage = "预留字段类型错误")]
+        [Display(Name = "预留字段"), StringLength(40), ANC(ErrorMessage = "预留字段 类型错误")]
         public string ReservedField { get; set; }
     }
 }
