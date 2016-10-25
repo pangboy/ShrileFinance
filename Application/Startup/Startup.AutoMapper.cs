1fe3421fcd5c07f7f93d5a7d16bee397ee00d3ff
foolishqi <foolishqi@126.com>
2016-10-25 15:21:43 +0800

    增加自动映射功能

---
+++ Application/Startup/Startup.AutoMapper.cs
@@ -0,0 +1,20 @@
+﻿namespace Application
+{
+    using AutoMapper;
+    using Mappings;
+
+    /// <summary>
+    /// 自动映射的配置
+    /// </summary>
+    public partial class Startup
+    {
+        public void ConfigureMapper()
+        {
+            Mapper.Initialize(config =>
+            {
+                config.AddProfile<DomainToViewModelMappingProfile>();
+                config.AddProfile<ViewModelToDomainMappingProfile>();
+            });
+        }
+    }
+}
