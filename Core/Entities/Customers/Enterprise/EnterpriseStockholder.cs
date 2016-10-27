51f29e7372682a7c3505b901efb8ead1de9ad47c
foolishqi <foolishqi@126.com>
2016-10-27 13:20:47 +0800

    代码规范整理

---
+++ Core/Entities/Customers/Enterprise/EnterpriseStockholder.cs
@@ -0,0 +1,28 @@
+﻿namespace Core.Entities.Customers.Enterprise
+{
+    /// <summary>
+    /// 企业股东
+    /// </summary>
+    public class EnterpriseStockholder : Stockholder, IEnterprise
+    {
+        /// <summary>
+        /// 登记注册类型
+        /// </summary>
+        public string RegistraterType { get; set; }
+
+        /// <summary>
+        /// 登记注册号码
+        /// </summary>
+        public string RegistraterCode { get; set; }
+
+        /// <summary>
+        /// 组织机构代码
+        /// </summary>
+        public string OrganizateCode { get; set; }
+
+        /// <summary>
+        /// 机构信用代码
+        /// </summary>
+        public string InstitutionCreditCode { get; set; }
+    }
+}
---
+++ Data/ModelConfigurations/EnterpriseStockholderConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace Data.ModelConfigurations
+{
+    using System.Data.Entity.ModelConfiguration;
+    using Core.Entities.Customers.Enterprise;
+
+    public class EnterpriseStockholderConfiguration : EntityTypeConfiguration<EnterpriseStockholder>
+    {
+        public EnterpriseStockholderConfiguration()
+        {
+            Property(m => m.RegistraterType).HasMaxLength(2);
+            Property(m => m.RegistraterCode).HasMaxLength(20);
+            Property(m => m.OrganizateCode).HasMaxLength(10);
+            Property(m => m.InstitutionCreditCode).HasMaxLength(18);
+        }
+    }
+}
