950a4f1b0b79380672e3038872ce406677c3ddf8
foolishqi <foolishqi@126.com>
2016-10-30 16:31:34 +0800

    删除测试项目

--- ShrileFinance/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ShrileFinance.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
