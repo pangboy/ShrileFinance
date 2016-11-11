abc4957efbd678ed540c34d4196b312ff7b9d32b
七月 <foolishqi@126.com>
2016-11-11 17:33:39 +0800

    增加合作商

---
+++ Data/Repositories/PartnerRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace Data.Repositories
+{
+    using System;
+    using System.Linq;
+    using Core.Entities;
+    using Core.Entities.Partner;
+    using Core.Interfaces.Repositories;
+
+    public class PartnerRepository : BaseRepository<Partner>, IPartnerRepository
+    {
+        public PartnerRepository(MyContext context) : base(context)
+        {
+        }
+
+        public Partner GetByUser(AppUser user)
+        {
+            // TODO: 模拟数据
+            var approvers = Context.Set<AppUser>().GroupBy(m => m.RoleId).Select(m => m.First()).ToList();
+
+            return new Partner {
+                Approvers = approvers
+            };
+        }
+    }
+}
