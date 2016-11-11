6da6173014851385e0dcddd23f2c0c100ac405d9
邹强鲁 <Thinkxuhao@outlook.com>
2016-11-11 17:01:30 +0800

    提交丢失文件

---
+++ Data/ModelConfigurations/FinanceExtensionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace Data.ModelConfigurations
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.ModelConfiguration;
+    using Core.Entities.Finance;
+
+    public class FinanceExtensionConfiguration:EntityTypeConfiguration<FinanceExtension>
+    {
+        public FinanceExtensionConfiguration()
+        {
+            // 主键
+            HasKey(m => m.Id);
+            Property(m => m.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+
+            ToTable("FANC_FinanceExtension");
+        }
+    }
+}
