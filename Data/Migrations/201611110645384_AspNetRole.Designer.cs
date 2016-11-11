8d81bfaf67f8fa8fb9f4caafdbd65784cfab73fd
七月 <foolishqi@126.com>
2016-11-11 16:38:14 +0800

    合并代码

--- Data/Migrations/201611110645384_AspNetRole.Designer.cs
+++ Data/Migrations/201611110645384_AspNetRole.Designer.cs
@@ -7,13 +7,13 @@ namespace Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class Flow : IMigrationMetadata
+    public sealed partial class AspNetRole : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Flow));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(AspNetRole));
         
         string IMigrationMetadata.Id
         {
-            get { return "201611060427119_Flow"; }
+            get { return "201611110645384_AspNetRole"; }
         }
         
         string IMigrationMetadata.Source
--- Data/Migrations/201611110645384_AspNetRole.cs
+++ Data/Migrations/201611110645384_AspNetRole.cs
@@ -3,7 +3,7 @@ namespace Data.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class UseIdentity : DbMigration
+    public partial class AspNetRole : DbMigration
     {
         public override void Up()
         {
