add7d7e9e3658a925f483f68df9be28067ae1393
邹强鲁 <Thinkxuhao@outlook.com>
2016-11-15 09:38:12 +0800

    解决冲突

---
+++ UsedCarsFinance/Web/Controllers/Finance/OperationController.cs
@@ -0,0 +1,51 @@
+﻿namespace Web.Controllers.Finance
+{
+    using System;
+    using System.Web.Http;
+    using Application;
+    using Application.ViewModels.FinanceViewModels;
+
+    /// <summary>
+    /// 运营
+    /// </summary>
+    public class OperationController : ApiController
+    {
+        private readonly FinanceAppService financeAppService;
+
+        public OperationController(FinanceAppService financeAppService)
+        {
+            this.financeAppService = financeAppService;
+        }
+
+        /// <summary>
+        /// 通过融资标识获取运营ViewModel
+        /// </summary>
+        /// <param name="financeId">融资标识</param>
+        /// <returns>融资审核ViewModel</returns>
+        [HttpGet]
+        public OperationViewModel GetOperation(Guid financeId)
+        {
+            var operationViewModel = financeAppService.GetOperationByFinanceId(financeId);
+
+            return operationViewModel;
+        }
+
+        /// <summary>
+        /// 编辑运营
+        /// </summary>
+        /// <param name="value">运营ViewModel</param>
+        /// <returns>处理结果</returns>
+        [HttpPost]
+        public IHttpActionResult EditOperation(OperationViewModel value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ValidModel.ShowErrorFirst(ModelState));
+            }
+
+            financeAppService.EditOperation(value: value);
+
+            return Ok(value);
+        }
+    }
+}
