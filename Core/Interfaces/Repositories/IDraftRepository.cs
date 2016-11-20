69489f8ad639f319fed851cb05a6c7e958907a51
七月 <foolishqi@126.com>
2016-11-20 17:10:01 +0800

    草稿功能

---
+++ Core/Interfaces/Repositories/IDraftRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Interfaces.Repositories
+{
+    using Entities.Other;
+
+    public interface IDraftRepository : IRepository<Draft>
+    {
+        Draft GetByUserAndPageLink(string userId, string pageLink);
+    }
+}
---
+++ UsedCarsFinance/Web/Controllers/Sys/DraftController.cs
@@ -0,0 +1,50 @@
+﻿namespace Web.Controllers.Sys
+{
+    using System.Web.Http;
+    using Application;
+    using Application.ViewModels.OtherViewModels;
+
+    public class DraftController : ApiController
+    {
+        private readonly DraftAppService service;
+
+        public DraftController(DraftAppService service)
+        {
+            this.service = service;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Save(DraftViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            service.Save(model);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IHttpActionResult Read(string pageLink)
+        {
+            var draft = service.Read(pageLink);
+
+            if (draft == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(draft);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Clear(string pageLink)
+        {
+            service.Clear(pageLink);
+
+            return Ok();
+        }
+    }
+}
