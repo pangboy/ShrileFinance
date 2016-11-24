30e00b79228ad78e9a309aedac1a54959f652f7b
严冬 <753045147@qq.com>
2016-11-24 18:52:22 +0800

    合同打印修改车辆查询采用视图操作

--- Data/Repositories/FinanceRepository.cs
+++ Data/Repositories/FinanceRepository.cs
@@ -28,7 +28,7 @@
                  ma.Name AS '[乙方姓名]', ma.[Identity] AS '[乙方证件号码]',
                  ja.Name AS '[共借人姓名]', ja.[Identity] AS '[共借人证件号码]',
                  fv.PlateNo AS '[车牌号]', fv.FrameNo AS '[车架号]', fv.EngineNo AS '[发动机号]', fv.RunningMiles AS '[里程数]',
-                 vvi.CarBrand AS '[品牌]', vvi.Series AS '[型号]',
+                 ch.CarBrand AS '[品牌]', ch.Series AS '[型号]',
                  '' AS '[融资额大写]', fi.Principal AS '[融资额]', 
                  pi.FinancingPeriods AS '[融资期限]', '' AS '[手续费大写]', fi.Poundage AS '[手续费]',
                  '' AS '[保证金大写]', (pi.CustomerBailRatio * fi.Principal)AS '[保证金]',
@@ -49,11 +49,7 @@
                     FROM FANC_Applicant AS ai
                     WHERE ai.FinanceId = @financeId AND Type = 2
                 ) AS ja ON ja.FinanceId = fi.Id
-               LEFT JOIN(
-                 SELECT sv.Vehicle, ss.Series, sb.CarBrand, sv.VehicleCode FROM ywcommondb.dbo.Sys_Vehicle AS sv
-                    LEFT JOIN ywcommondb.dbo.Sys_Series AS ss ON sv.SeriesCode = ss.SeriesCode
-                    LEFT JOIN ywcommondb.dbo.Sys_Brand AS sb ON sb.BrandCode = ss.BrandCode
-               ) AS vvi ON fv.VehicleKey = vvi.VehicleCode
+               LEFT JOIN V_CarHome AS ch ON fv.VehicleKey  = ch.VehicleCode
                LEFT JOIN(
                 SELECT Number, FinanceId FROM FANC_Contact AS fc WHERE fc.FinanceId = @financeId AND Name = '融资租赁合同'
                ) AS rz ON rz.FinanceId = fi.Id
