8f765275ea6f6a42cb6ab6aa4c6d137a53981836
邹强鲁 <Thinkxuhao@outlook.com>
2016-11-25 13:08:52 +0800

    影像上传

--- UsedCarsFinance/Web/Finance/Content/FinanceJs.js
+++ UsedCarsFinance/Web/Finance/Content/FinanceJs.js
@@ -86,8 +86,10 @@ function FieldsetLoadDateFactory(name, type, data) {
             // 加载数据 table
             $(data).each(function (index, item) {
                 // 加载数据 table
-                $("fieldset[name=" + name + "]").find("[id=" + name + "_dg" + "]").datagrid('appendRow', item);;
+                $("fieldset[name=" + name + "]").find("[id=" + name + "_dg" + "]").datagrid('appendRow', item);
             });
+
+            $("fieldset[name=" + name + "]").find("[id=" + name + "_dg" + "]").datagrid('loadData', data);
         }
     }
 
