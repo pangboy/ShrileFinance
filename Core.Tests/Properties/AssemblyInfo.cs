a79a95e23e729e38857bca97941b9c7d9bdc6dfc
七月 <foolishqi@126.com>
2016-11-30 15:57:18 +0800

    移除 Application 项目的单元测试并增加 Core 项目的单元测试.

--- Core.Tests/Properties/AssemblyInfo.cs
+++ Core.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Application.Test")]
+[assembly: AssemblyTitle("Core.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Application.Test")]
+[assembly: AssemblyProduct("Core.Tests")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("72febeab-7b78-4c7c-b73a-e5095032afad")]
+[assembly: Guid("8c3ca377-21ee-46ae-b6c3-988074822cdf")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
