d81493c91d14e02c7f22aa8b892079c59a737622
七月 <foolishqi@126.com>
2016-12-01 13:40:32 +0800

    增加参数超出预计范围异常.

---
+++ Core/Exceptions/ArgumentOutOfRangeAppException.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Exceptions
+{
+    using System;
+
+    class ArgumentOutOfRangeAppException : AppException
+    {
+        public ArgumentOutOfRangeAppException(string message = null, Exception innerException = null) : base(message, innerException)
+        {
+        }
+    }
+}
