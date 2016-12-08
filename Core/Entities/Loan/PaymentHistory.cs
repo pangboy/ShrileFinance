6b550db29366043769d24eaffd96e05de58f778b
七月 <foolishqi@126.com>
2016-12-08 11:53:26 +0800

    借据和还款记录模型 构造函数私有化.

--- Core/Entities/Loan/PaymentHistory.cs
+++ Core/Entities/Loan/PaymentHistory.cs
@@ -9,10 +9,6 @@
     /// </summary>
     public class PaymentHistory : Entity, IAggregateRoot
     {
-        public PaymentHistory()
-        {
-        }
-
         public PaymentHistory(
             decimal scheduledPaymentPrincipal,
             decimal scheduledPaymentInterest,
@@ -34,6 +30,10 @@
             ActualPaymentPrincipal = actualPaymentPrincipal;
             ActualPaymentInterest = actualPaymentInterest;
             DatePayment = DateTime.Now;
+        }
+
+        private PaymentHistory()
+        {
         }
 
         /// <summary>
